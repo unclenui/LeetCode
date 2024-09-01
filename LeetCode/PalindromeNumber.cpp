f33d30fb0cbb941a01cb39dc1ff1a10b604ae51e
BaoKhongRach <tienson19273@gmail.com>
2024-09-01 23:17:05 +0700

    Add 2 problem-answers

---
+++ LeetCode/PalindromeNumber.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        if (x == 0) return true;
+        string s = "";
+        while (x > 0) {
+            s += (char)(x%10+(int)'0');
+            x/=10;
+        }
+        for (int i = 0; i <= (s.length()-1)/2; i++) 
+            if (s[i] != s[s.length()-1-i]) return false;
+        return true;
+    }
+};
